a1f3ae3850490fb1e259135449b6df17d84e2d9d
Sébastien Méric <sebastien.meric@gmail.com>
2020-09-24 11:03:06 +0200

    Update gateway.php
    Commentaires + suppression de valeurs de test

--- public/gateway.php
+++ public/gateway.php
@@ -1,35 +1,46 @@
 <?php
+/**
+ * Fichier appelé en ajax depuis le fichier /public/js/main.js.
+ * Son rôle est de générer une réponse au format json qui corespondra aux commande et données qui lui seront envoyées en POST.
+ */
+
+// La réponse sera formatée en json
 header("Content-type: application/json; charset=utf-8");
 
+// autoloader des classes php utilisées
 require __DIR__ . '/vendor/autoload.php';
+// Informations de connexion à la base MySQL
 require __DIR__ . '/config.php';
-//require __DIR__ . '/init.php';
+// Création de la table lors de la 1ère utilisation.
+// A desactiver après la 1ère utilisation.
+require __DIR__ . '/init.php';
 
-//use \SMeric\Utilities\DB;
+// Espace de nommage des classes instanciées
 use \SMeric\Apps\Memory\ScoresManager;
 use \SMeric\Apps\Memory\ScoreControler;
 use \SMeric\Apps\Memory\ScoresViewer;
 
-// Le "value" est un nombre :
+// La "value" est un nombre reçu en POST par ce fichier lors de l'appel ajax :
 //  compris entre 30000 et 600000 pour un score
 //  compris entre 1 et 10 pour une liste de scores
-//  un entier si c'est un id d'enregistrement
+//  (pas utilisé ici :) un entier si c'est un id d'enregistrement
 $value = isset( $_POST['value'] ) ? $_POST['value'] : 0;
-$value = 100000;
-//$value = 6;
 
-// L'action peut être :
+// L'"action" est une chaine de caractères alphabétique reçu en POST par ce fichier lors de l'appel ajax.
+// Elle peut être :
 //  "add" : ajouter un score à la base de données
 //  "getList" : récupérer une liste de scores dans la base de données
-//  "update" ou "delete" : mettre à jour ou supprimer un score de la base de données (pas utilisé ici)
+//  (pas utilisé ici :) "get", "update" ou "delete" : récupérer un enregistrement, mettre à jour ou supprimer un score de la base de données
 $action = isset( $_POST['action'] ) ? $_POST['action'] : "";
-$action = "add";
-//$action = "getList";
 
-// Instanciation des 3 classes
-$model = new ScoresManager( $config );
+// Instanciation des 3 classes suivant le modèle MVC :
+// Les données utilisateur sont reçu par le controler qui se charge de les nétoyer, de les valider et de les passer au manager.
+// Les données sont inscrites ou lues en base et une réponse adaptée est générée par le manager.
+// La réponse générée au niveau du controler ou du manager est formatée et servie comme réponse à la requête ajax par le viewer.
+
+$model      = new ScoresManager( $config );
 $controller = new ScoreControler( $model, $value, $action );
-$view = new ScoresViewer( $controller, $model );
+$view       = new ScoresViewer( $controller, $model );
 
 // Réponse du serveur au format json
 die( $view->getJsonResponse() );
