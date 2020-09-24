af81d3a02fd5d32c71922bc525ed05d4724aeb20
Sébastien Méric <sebastien.meric@gmail.com>
2020-09-24 10:36:24 +0200

    Update init.php
    Traduction des messages en français et commentaires.

--- public/init.php
+++ public/init.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Ce fichier n'est destiné à être utilisé que lors de la mise en route de l'application.
+ * Son rôle est de créer, dans la base de données, la table dans laquelle seront enrigistrés les temps.
+ * Il est inclus dans le fichier gateway.php et devrait être desactivé une fois qu'il aura rempli son rôle.
+ * Les informations de connexion à la base se trouvent dans le fichier config.php également inclus dans le fichier gateway.php.
+ */
+
 if ( ! isset( $config ) )
     die();
 
@@ -7,22 +14,22 @@ $table = $config['table'];
 $conn = new mysqli( $config['host'], $config['user'], $config['pass'], $config['dbname'] );
 
 if ( mysqli_connect_error() ){
-    die( "Database connection failed: " . mysqli_connect_error() );
+    die( "La connexion à la base de données a échoué: " . mysqli_connect_error() );
 }
-echo "Connected successfully\n";
+echo "Succès de la connexion.\n";
 
 if ( $sql = file_get_contents( '../scores.sql' ) ){
     if ( $conn->query( $sql ) === true ){
-        echo "Table $table created successfully\n";
+        echo "La table $table a bien été créée.\n";
     }
     else {
-        die( "Error creating table: " . $conn->error );
+        die( "Erreur lors de la création de la table $table: " . $conn->error );
     }
 }
 else {
-    die( "Error opening sql file." );
+    die( "Erreur lors de la tetative d'ouverture et/ou de lecture du fichier sql." );
 }
 
 $conn->close();
 
-die( "\nrequire __DIR__ . '/init.php'; instruction should be removed from gateway.php once table `$table` was successfully created." );
+die( "\nL'instruction <code>require __DIR__ . '/init.php';</code> devrait être retirée du fichier gateway.php une fois la table '$table' créée." );
